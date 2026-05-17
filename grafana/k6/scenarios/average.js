8698ab1d7821f1a9e5f364e7bbc2f3e293f72b8b
Riccardo Rasera <riccardo.rasera.extern@adesso.it>
2026-05-17 16:25:05 +0200

    Refactor k6 test tagging to use consistent scenarioTag variable

--- grafana/k6/scenarios/average.js
+++ grafana/k6/scenarios/average.js
@@ -2,7 +2,7 @@ import {sleep} from "k6";
 import {searchAccounts, getAccountById, createAccount, deleteAccount, patchAccount} from "../requests/accounts.js";
 import {randomAccountPayload} from "../utils/dtoHelpers.js";
 
-const TAGS = {scenario: "average"};
+const scenarioTag = {scenario: "average"};
 
 export const options = {
     stages: [
@@ -19,17 +19,17 @@ export const options = {
     },
     tags: {
         project: "nodejs",
-        ...TAGS
+        ...scenarioTag
     }
 };
 
 export default function () {
     // Search a random page wit size 20
-    searchAccounts(TAGS);
+    searchAccounts(scenarioTag);
     sleep(1);
 
     // Create a random account
-    const account = JSON.parse(createAccount(randomAccountPayload(), TAGS).body);
+    const account = JSON.parse(createAccount(randomAccountPayload(), scenarioTag).body);
 
     if (!account?.id) {
         return;
@@ -38,14 +38,14 @@ export default function () {
     sleep(1);
 
     // Search the account previously created
-    getAccountById(account.id, TAGS);
+    getAccountById(account.id, scenarioTag);
     sleep(1);
 
     // Update account previously created
-    patchAccount(account.id, randomAccountPayload(), TAGS);
+    patchAccount(account.id, randomAccountPayload(), scenarioTag);
     sleep(1);
 
     // Delete account previously created
-    deleteAccount(account.id, TAGS)
+    deleteAccount(account.id, scenarioTag)
     sleep(1);
 }
